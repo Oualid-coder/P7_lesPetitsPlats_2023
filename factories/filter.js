03e9f12442875cfa4f454155d9b94e02c6956c84
oualid <oualid@brokeandabroad.com>
2024-09-26 11:29:02 +0200

    added new feature

--- factories/filter.js
+++ factories/filter.js
@@ -1,7 +1,8 @@
+import DOMPurify from 'dompurify';
 
     export function filterFactory(recipes, callback) {
         function filterRecipes(searchText) {
-            const motCle = searchText.toLowerCase();
+            const motCle = DOMPurify.sanitize(searchText.toLowerCase()); // Nettoie l'input utilisateur
             const resultats = motCle === "" ?
                 recipes :
                 recipes.filter(recette => 
@@ -16,37 +17,3 @@
     }
 
 
-
-    // export function filterFactory(recipes, callback) {
-    //     function filterRecipes(searchText) {
-    //         const motCle = searchText.toLowerCase();
-    //         let resultats = [];
-
-    //         if (motCle === "") {
-    //             resultats = recipes;
-    //         } else {
-    //             for (let i = 0; i < recipes.length; i++) {
-    //                 const recette = recipes[i];
-    //                 let nameMatch = recette.name.toLowerCase().includes(motCle);
-    //                 let descriptionMatch = recette.description.toLowerCase().includes(motCle);
-    //                 let ingredientsMatch = false;
-
-    //                 let j = 0;
-    //                 while (j < recette.ingredients.length && !ingredientsMatch) {
-    //                     if (recette.ingredients[j].ingredient.toLowerCase().includes(motCle)) {
-    //                         ingredientsMatch = true;
-    //                     }
-    //                     j++;
-    //                 }
-
-    //                 if (nameMatch || descriptionMatch || ingredientsMatch) {
-    //                     resultats.push(recette);
-    //                 }
-    //             }
-    //         }
-
-    //         callback(resultats);
-    //     }
-
-    //     return { filterRecipes };
-    // }
